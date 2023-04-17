3cf35da505f1814b398defb45ca304407a6c16c8
Mac May <tmcpbmay@gmail.com>
2023-04-17 18:44:31 -0400

    Added the roster page

---
+++ resources/views/roster.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Wash RFC Roster</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>The Men of Washington RFC</h1>
+        <h3>Coaching Staff</h3>
+        <h3>The Players</h3>
+        <!-- Need some kind of box all the players will go in -->
+        <!-- Set it up as a flex so it's responsive -->
+            <!-- Then each player. -->
+            <!-- A picture -->
+            <!-- Their name -->
+
+    </body>
+</html>
